6a05f1507c6eab54437a55a61b080ee154e5dc49
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-03-12 17:42:38 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,4 +1,8 @@
 import os, sys
+try:os.system("clear")
+except:pass
+os.system('termux-setup-storage')
+os.system('git pull')
 os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
 try:
     __import__("RANDOM").menu()
