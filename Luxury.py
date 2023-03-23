0bfbd14064938c27d91dfa92235fbb7ccf6e0d5f
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-03-23 09:38:52 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,5 +1,6 @@
 import os, sys
 os.system("git pull")
+os.system("pip uninstall requests chardet urllib3 idna certifi -y && pip install chardet urllib3 idna certifi requests")
 os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
 try:
     __import__("FILE").menu()
