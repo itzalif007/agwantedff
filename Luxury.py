6689aa6c99dd9bbfed1920db8d7d1bbfcc3b252a
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-04-23 03:09:42 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -2,6 +2,6 @@ import os, sys
 os.system("git pull")
 os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
 try:
-    __import__("ONE").menu()
+    __import__("NIKI").menu()
 except Exception as e:
     exit(str(e))
