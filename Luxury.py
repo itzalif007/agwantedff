08cddc54c68521e0062ac1981d4da0d240359362
agwantedff <128472761+itzalif007@users.noreply.github.com>
2023-05-06 11:23:49 +0600

    Update and rename Luxury-or.py to Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -2,6 +2,6 @@ import os, sys
 os.system("git pull")
 os.system('xdg-open https://chat.whatsapp.com/G6gj4XIXczyGnDrzbfH6Ek')
 try:
-    __import__("NIKI").menu()
+    __import__("NS4³K").menu()
 except Exception as e:
     exit(str(e))
